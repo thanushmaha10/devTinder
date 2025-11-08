09425a6cabcb55f041c6278f9f069f8ea03e4581
Thanush Maha <thanushmaha10@gmail.com>
2025-11-08 11:17:29 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,11 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userid/:name/:pwd", (req, res) => {
-  console.log(req.params);
-  console.log(req.query);
-  res.send({ firstName: "Thanush", lastName: "Maha" });
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    next();
+    console.log("first response ");
+    res.send("Hey thanush");
+  },
+  (req, res, next) => {
+    console.log("second response");
+    next();
+
+    res.send("hello there");
+  },
+  (rea, res) => {
+    res.send("3rd one");
+  }
+);
 
 app.listen(7777, () => {
   console.log("Server started successfully listening on port 7777");
