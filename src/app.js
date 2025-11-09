8f71e7c4f68352a378c91484bf2ced8d9ef2eaad
Thanush Maha <thanushmaha10@gmail.com>
2025-11-09 17:50:32 +0530

    Dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -4,18 +4,14 @@ const User = require("./models/user");
 
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Thanush",
-    lastNme: "Maha",
-    email: "thanush@maha.com",
-    password: "thanush@123",
-    age: 23,
-    gender: "M",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
+    console.log(req.body);
     res.send("User added successfully");
   } catch (err) {
     res.status(400).send("Error saving the data" + err.message);
